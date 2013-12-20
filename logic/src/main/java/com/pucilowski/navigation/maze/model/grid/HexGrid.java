82cffc2066edaca1e47f5ba9bb913554ab115730
martin <pucilowski.m@gmail.com>
2013-12-20 01:53:26 +0000

    uh

--- logic/src/main/java/com/pucilowski/navigation/maze/model/grid/HexGrid.java
+++ logic/src/main/java/com/pucilowski/navigation/maze/model/grid/HexGrid.java
@@ -1,6 +1,7 @@
 package com.pucilowski.navigation.maze.model.grid;
 
 import com.pucilowski.navigation.maze.model.Cell;
+import com.pucilowski.navigation.maze.model.grid.paint.DoublePoint;
 import com.pucilowski.navigation.maze.model.grid.paint.GridPaint;
 
 import java.awt.*;
@@ -11,13 +12,13 @@ import java.awt.*;
 public class HexGrid extends Grid {
 
     public static final Point[] offsets = {
-            new Point(1, 0), // ne
-            new Point(1, 1), // se
+            new Point(1, -1), // ne
+            new Point(1, 0), // se
 
             new Point(0, 1), // s
 
-            new Point(-1, 1), // sw
-            new Point(-1, 0), // nw
+            new Point(-1, 0), // sw
+            new Point(-1, -1), // nw
 
             new Point(0, -1), // n
     };
@@ -38,6 +39,87 @@ public class HexGrid extends Grid {
         if (cell.x % 2 == 1) y++;
 
         return new Point(x, y);
+    }
+
+    // paint
+
+    double triangleWidth = 0.5D;
+    double triangleHeight = 0.75D;
+    //double triangleHeight = Math.sin(Math.toRadians(60));
+    //double triangleHeight = 1D;
+
+    double leftX = 0D;
+    double rightX = 1D;
+
+    double farLeftX = -0.5D;
+    double farRightX = 1.5D;
+
+    double topY = 0;
+    double midY = triangleHeight;
+    double bottomY = 2 * triangleHeight;
+
+    DoublePoint[] points = {
+            new DoublePoint(rightX, topY),
+            new DoublePoint(farRightX, midY),
+            new DoublePoint(rightX, bottomY),
+            new DoublePoint(leftX, bottomY),
+            new DoublePoint(farLeftX, midY),
+            new DoublePoint(leftX, topY),
+    };
+
+
+    @Override
+    public Point getLocation(Cell cell, int size) {
+        //size/=2;
+
+        double px = triangleWidth + cell.x + cell.x * triangleWidth;
+        double py = 2 * triangleHeight * cell.y;
+
+        if (cell.x % 2 == 1) py += triangleHeight;
+
+        int x = (int) (px * size);
+        int y = (int) (py * size);
+
+        return new Point(x, y);
+    }
+
+    @Override
+    public Point getPoint(Cell cell, int index, int size) {
+        //size/=2;
+        DoublePoint dp = points[index];
+
+        int x = (int) (dp.x * size);
+        int y = (int) (dp.y * size);
+
+        return new Point(x, y);
+    }
+
+    @Override
+    public Polygon getSide(Cell cell, int index, int size) {
+        //size/=2;
+        Point a = getPoint(cell, index, size);
+        Point b = getPoint(cell, (index + 1) % sides, size);
+
+        return new Polygon(
+                new int[]{a.x, b.x},
+                new int[]{a.y, b.y},
+                2
+        );
+    }
+
+    @Override
+    public Polygon getPolygon(Cell cell, int size) {
+        //size/=2;
+        int[] xs = new int[sides];
+        int[] ys = new int[sides];
+        for (int i = 0; i < sides; i++) {
+            Point p = getPoint(cell, i, size);
+
+            xs[i] = p.x;
+            ys[i] = p.y;
+        }
+
+        return new Polygon(xs, ys, sides);
     }
 
 
--- logic/src/main/java/com/pucilowski/navigation/maze/model/grid/SquareGrid.java
+++ logic/src/main/java/com/pucilowski/navigation/maze/model/grid/SquareGrid.java
@@ -3,7 +3,6 @@ package com.pucilowski.navigation.maze.model.grid;
 import com.pucilowski.navigation.maze.model.Cell;
 
 import java.awt.*;
-import java.util.ArrayList;
 
 /**
  * Created by martin on 10/12/13.
@@ -11,10 +10,10 @@ import java.util.ArrayList;
 public class SquareGrid extends Grid {
 
     public static final Point[] offsets = {
-            new Point(0, -1),
-            new Point(1, 0),
-            new Point(0, 1),
-            new Point(-1, 0),
+            new Point(0, -1), // n
+            new Point(1, 0), // e
+            new Point(0, 1), // s
+            new Point(-1, 0), // w
     };
 
     public SquareGrid(int width, int height) {
@@ -24,5 +23,53 @@ public class SquareGrid extends Grid {
     @Override
     public Point getOffset(Cell cell, int index) {
         return offsets[index];
+    }
+
+    @Override
+    public Point getLocation(Cell cell, int size) {
+        int px = size * cell.x;
+        int py = size * cell.y;
+
+        return new Point(px, py);
+    }
+
+    @Override
+    public Point getPoint(Cell cell, int index, int size) {
+        return null;
+    }
+
+    @Override
+    public Polygon getSide(Cell cell, int index, int size) {
+        Point p = getOffset(cell, index);
+
+        // (0,0),(1,0)
+        // (1,0),(1,1)
+        // (1,1),(0,1)
+        // (0,1),(0,0)
+
+        //
+
+        Point[] points = {
+                new Point(0, 0),
+                new Point(1, 0),
+                new Point(1, 1),
+                new Point(0, 1)
+        };
+
+
+        Point a = points[index];
+        Point b = points[(index + 1) % sides];
+
+        return new Polygon(
+                new int[]{a.x*size, b.x*size},
+                new int[]{a.y*size, b.y*size},
+                2
+        );
+
+    }
+
+    @Override
+    public Polygon getPolygon(Cell cell, int index) {
+        return null;
     }
 }
